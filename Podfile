7bf08cd827d4ae1fed5209b15b3e039c717da081
Vinh Nguyen <blacksunol@yahoo.com>
2018-03-16 15:10:19 +0700

    add Google Auth

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'mixmax' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for mixmax
+  pod 'GoogleSignIn'
+  target 'mixmaxTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'mixmaxUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ mixmax/Google/GoogleLoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GoogleLoginViewController.swift
+//  mixmax
+//
+//  Created by Vinh Nguyen on 2/9/18.
+//  Copyright © 2018 Vinh Nguyen. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate  {
+    
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        
+        // Uncomment to automatically sign in the user.
+        //GIDSignIn.sharedInstance().signInSilently()
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
+    }
+    
+    // Implement these methods only if the GIDSignInUIDelegate is not a subclass of
+    // UIViewController.
+    
+    // Stop the UIActivityIndicatorView animation that was started when the user
+    // pressed the Sign In button
+    func signInWillDispatch(signIn: GIDSignIn!, error: NSError!) {
+//        UIActivityIndicatorView.stopAnimating()
+    }
+    
+    // Present a view that prompts the user to sign in with Google
+    func sign(_ signIn: GIDSignIn!,
+              present viewController: UIViewController!) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    // Dismiss the "Sign in with Google" view
+    func sign(_ signIn: GIDSignIn!,
+              dismiss viewController: UIViewController!) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+
+    
+}
