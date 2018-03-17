ee91ba4c6b25d547581bdc4f8b017be45b51d31c
Vinh Nguyen <blacksunol@yahoo.com>
2018-03-17 23:42:24 +0700

    google drive list files

--- mixmax/Google/GoogleLoginViewController.swift
+++ mixmax/Google/GoogleLoginViewController.swift
@@ -8,28 +8,57 @@
 
 import UIKit
 import GoogleSignIn
+import GoogleAPIClientForREST
 
-class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate  {
+
+class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate  {
+    
+    private let scopes = [kGTLRAuthScopeDriveReadonly]
     
     @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    private let service = GTLRDriveService()
+    
+    let output = UITextView()
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Configure Google Sign-in.
+        GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().scopes = scopes
+        GIDSignIn.sharedInstance().signInSilently()
         
-        // Uncomment to automatically sign in the user.
-        //GIDSignIn.sharedInstance().signInSilently()
+        // Add the sign-in button.
+        view.addSubview(signInButton)
         
-        // TODO(developer) Configure the sign-in button look/feel
-        // ...
+        // Add a UITextView to display output.
+        output.frame = view.bounds
+        output.isEditable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        output.isHidden = true
+        view.addSubview(output);
+
     }
     
-    // Implement these methods only if the GIDSignInUIDelegate is not a subclass of
-    // UIViewController.
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            self.service.authorizer = nil
+        } else {
+            self.signInButton.isHidden = true
+            self.output.isHidden = false
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+            listFiles()
+        }
+    }
     
-    // Stop the UIActivityIndicatorView animation that was started when the user
-    // pressed the Sign In button
     func signInWillDispatch(signIn: GIDSignIn!, error: NSError!) {
 //        UIActivityIndicatorView.stopAnimating()
     }
@@ -46,6 +75,51 @@ class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate  {
         self.dismiss(animated: true, completion: nil)
     }
 
+    // List up to 10 files in Drive
+    func listFiles() {
+        let query = GTLRDriveQuery_FilesList.query()
+        query.pageSize = 10
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:))
+        )
+    }
 
+    // Process the response and display output
+    func displayResultWithTicket(ticket: GTLRServiceTicket,
+                                 finishedWithObject result : GTLRDrive_FileList,
+                                 error : NSError?) {
+        
+        if let error = error {
+            showAlert( title: "Error", message: error.localizedDescription)
+            return
+        }
+        
+        var text = "";
+        if let files = result.files, !files.isEmpty {
+            text += "Files:\n"
+            for file in files {
+                text += "\(file.name!) (\(file.identifier!))\n"
+            }
+        } else {
+            text += "No files found."
+        }
+        output.text = text
+    }
     
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
 }
