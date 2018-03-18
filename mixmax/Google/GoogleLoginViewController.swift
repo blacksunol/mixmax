f6be3e7117a7e4f1709f5bfe6b1affc41c8460a1
Vinh Nguyen <blacksunol@yahoo.com>
2018-03-18 22:57:30 +0700

    google drive get and play files

--- mixmax/Google/GoogleLoginViewController.swift
+++ mixmax/Google/GoogleLoginViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import GoogleSignIn
 import GoogleAPIClientForREST
+import AVFoundation
 
 
 class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate  {
@@ -18,6 +19,8 @@ class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignI
     @IBOutlet weak var signInButton: GIDSignInButton!
     
     private let service = GTLRDriveService()
+    
+    var player:AVPlayer?
     
     let output = UITextView()
 
@@ -56,6 +59,22 @@ class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignI
             self.output.isHidden = false
             self.service.authorizer = user.authentication.fetcherAuthorizer()
             listFiles()
+            print("Access token: \(user.authentication.accessToken)")
+            guard let accessToken = user.authentication.accessToken else {
+                return
+            }
+//            getFile()
+      
+            let headers = ["Authorization": "Bearer \(accessToken)"]
+            let url = URL(string: "https://www.googleapis.com/drive/v3/files/1siQAS6GbPTvN0xNKejBlyE6VREYa8hKC?alt=media")!
+            
+//            let url = URL(string: "https://www.googleapis.com/drive/v2/files/1siQAS6GbPTvN0xNKejBlyE6VREYa8hKC?alt=media")!
+            
+//            let url =  URL(string: "https://doc-0s-34-docs.googleusercontent.com/docs/securesc/669fcdtp0silioo506rd5psg3ut8s79n/v8am2bna2m1nbbg9v4o8qbr7fdsftpp7/1521381600000/02980703435378377253/02980703435378377253/1siQAS6GbPTvN0xNKejBlyE6VREYa8hKC?e=download&gd=true")!
+            let avAsset = AVURLAsset(url: url, options: ["AVURLAssetHTTPHeaderFieldsKey": headers])
+            let playerItem = AVPlayerItem(asset: avAsset)
+            player = AVPlayer(playerItem: playerItem)
+            player?.play()
         }
     }
     
@@ -83,6 +102,8 @@ class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignI
                              delegate: self,
                              didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:))
         )
+        
+        
     }
 
     // Process the response and display output
@@ -100,12 +121,35 @@ class GoogleLoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignI
             text += "Files:\n"
             for file in files {
                 text += "\(file.name!) (\(file.identifier!))\n"
+                print("filename: \(file.name!), fileid : \(file.identifier!)")
             }
         } else {
             text += "No files found."
         }
         output.text = text
     }
+    
+    // Process the response and display output
+    func getFileResultWithTicket(ticket: GTLRServiceTicket,
+                                 finishedWithObject file : GTLRDataObject,
+                                 error : NSError?) {
+        if let error = error {
+            showAlert( title: "Error", message: error.localizedDescription)
+            return
+        }
+        
+    }
+    
+    func getFile() {
+        let query = GTLRDriveQuery_FilesGet.query(withFileId: "1HCzFMnt9zBZEjGvFtTjeNPYz9fU6Ijmav6SrPT-rB7k")
+        
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinish: #selector(getFileResultWithTicket(ticket:finishedWithObject:error:))
+        )
+    }
+    
+    
     
     // Helper for showing an alert
     func showAlert(title : String, message: String) {
