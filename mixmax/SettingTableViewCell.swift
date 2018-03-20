2fcbe71304dc3bff0dc9fd453983f569d387245f
Vinh Nguyen <blacksunol@yahoo.com>
2018-03-20 10:51:32 +0700

    Add setting controller

---
+++ mixmax/SettingTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewCell.swift
+//  mixmax
+//
+//  Created by Vinh Nguyen on 3/20/18.
+//  Copyright © 2018 Vinh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+    
+}
